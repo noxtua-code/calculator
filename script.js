dc613b714bfb555b1d28016219e440a97ed7f5ce
noxtua-code <93567294+noxtua-code@users.noreply.github.com>
2023-01-21 15:37:47 +1000

    Added active button css and selected DOM elements in script file

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('.number');
+const screen = document.querySelector('.output');
+const specialButtons = document.querySelectorAll('.special');
+const operatorButtons = document.querySelectorAll('.operator');
+const decimalButton = document.querySelector('.decimal');
+
+
+
